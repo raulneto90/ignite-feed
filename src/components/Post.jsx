f768de82633008eb81737d944e035acc022b121a
Raul Neto <raulneto90@gmail.com>
2023-01-11 09:55:11 -0300

    feat: implemented post and comment section

---
+++ src/components/Post.jsx
@@ -0,0 +1,44 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/raulneto90.png" />
+          <div className={styles.authorInfo}>
+            <strong>Raul Neto</strong>
+            <span>Fullstack developer</span>
+          </div>
+        </div>
+
+        <time title="10 de Janeiro as 15:30" dateTime='2023-01-10 15:30:00'>Publicado há 1 hora</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p><a href="">👉 {' '} jane.design/doctorcare</a></p>
+
+        <p>
+          <a href="">#novoprojeto</a> {' '}
+          <a href="">#nlw</a> {' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
+    </article>
+  )
+}
