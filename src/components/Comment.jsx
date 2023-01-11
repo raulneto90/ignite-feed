f0661f562f664d622f22614dbdd2898fdde9c392
Raul Neto <raulneto90@gmail.com>
2023-01-11 12:22:11 -0300

    feat: comment list

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/raulneto90.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Raul Neto</strong>
+              <time title="10 de Janeiro as 15:30" dateTime='2023-01-10 15:30:00'>Cerca de 1 hora atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>
+            Muito bom Devon, parabéns!! 👏👏
+          </p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
